6116248d6af19e7f27aef6d32dc73b2e82d3195e
lilcobby <jshairfield13@gmail.com>
2023-10-29 19:15:49 -0400

    linked files and made some questions

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateShape = require("./lib/shape");
 const questions = [
   {
     type: "input",
-    name: "title",
-    message: "What is your project title?",
+    name: "text",
+    message:
+      "Please input at most three characters to represent your logo text",
+  },
+  {
+    type: "input",
+    name: "text-color",
+    message: "Please enter a text color",
+  },
+  {
+    type: "list",
+    name: "shape",
+    message: "Please select a shape for your logo",
+    choices: ["Circle", "Triangle", "Square"],
+  },
+  {
+    type: "input",
+    name: "shape-color",
+    message: "Please enter a shape color",
   },
 ];
+// runs prompt
 function prompts() {
-  inquirer.prompt(questions).then((answers) => {});
+  inquirer.prompt(questions).then((answers) => {
+    const test = generateShape(answers);
+    fs.writeFile(`./examples/${answers.shape}.svg`, test, (err) =>
+      err ? console.log(err) : console.log("file created")
+    );
+  });
 }
 function init() {
   prompts();
