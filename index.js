e4fb7a664999d94cea80729f8e8b6ad0513830ee
lilcobby <jshairfield13@gmail.com>
2023-10-31 21:05:32 -0400

    just need to make the test

--- index.js
+++ index.js
@@ -8,22 +8,12 @@ const questions = [
     name: "text",
     message:
       "Please input at most three characters to represent your logo text",
-    validate: function (choice){ 
-      var done = this.async();
-      if(choice.length > 3){
-        done(" Please only pick 3 character")
-        return;
-      }
-      if(choice.length === 0){
-        done("Please enter 3 characters")
-        return;
-      }
-      done(null,  true)
-      return;
-
-      //return choice.length <= 3 || "please only pick 3 letters"
-    }
-  
+    validate: function (choice) {
+      return (
+        (choice.length <= 3 && choice.length > 0) ||
+        "please pick at least 1 and no more than 3 character (s) "
+      );
+    },
   },
   {
     type: "input",
@@ -44,21 +34,12 @@ const questions = [
 ];
 // runs prompt
 function prompts() {
-  // conditionals go here?
   inquirer.prompt(questions).then((answers) => {
     const test = generateShape(answers);
-    
-    // if (answers.text.length > 3 || answers.text.length == 0) {
-    //   return console.log(
-    //     "Please enter a text length of at most 3 characters and at least 1 character."
-    //   );
-    // } else if (answers.shape_color !== "blue") {
-    //   return console.log("your color input is not a color, run program again");
-    // } else {
+
     fs.writeFile(`./examples/${answers.shape}.svg`, test, (err) =>
       err ? console.log(err) : console.log("file created")
     );
-      
   });
 }
 function init() {
